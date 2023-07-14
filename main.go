b95bfeca990e3c74f27206ef1429276dacf45334
tranminhwang <j2team.tranminhquang@gmail.com>
2023-07-14 16:12:28 +0700

    add fetchUrlsConcurrently example

--- main.go
+++ main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
 func main() {
 	// webServiceGin()
+	// lissajous()
+	// fetchUrlsConcurrently()
+}
+
+func fetchUrlsConcurrently() {
+	start := time.Now()
+	ch := make(chan string)
+
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	nBytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+
+	if err != nil {
+		ch <- fmt.Sprintf("while fetching %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nBytes, url)
 }
